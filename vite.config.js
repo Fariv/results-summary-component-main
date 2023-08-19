ca219aed11e3f3da19a0ccae5df36375641d0cb8
Ashraful Ferdous <ashraful@boo2.nl>
2023-08-19 13:13:06 +0600

    data.json is put in public, so vite plugin removed.

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
-import { defineConfig, normalizePath } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        viteStaticCopy({
-            targets: [
-                {
-                    src: normalizePath(path.resolve(__dirname) + '/data.json'),
-                    dest: normalizePath(path.resolve(__dirname) + '/dist'),
-                },
-            ],
-        }),
-    ],
+    plugins: [react()],
 })
